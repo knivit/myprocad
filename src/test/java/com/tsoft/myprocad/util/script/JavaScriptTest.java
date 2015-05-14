dc4fca73c23b1ac0c71dd06537494a0b6ce87102
knivit <knivit@gmail.com>
2015-05-14 20:12:35 +0300

    coding

--- src/test/java/com/tsoft/myprocad/util/script/JavaScriptTest.java
+++ src/test/java/com/tsoft/myprocad/util/script/JavaScriptTest.java
@@ -8,19 +8,19 @@ public class JavaScriptTest extends AbstractItemTest {
     public void calcFirstFloor() throws Exception {
         JavaScript js = new JavaScript();
 
-     //   js.addBinding("Application", new ApplicationBinding(js));
-     //   js.executeScript("lib/js/Деденево/3D/Модель3D.js");
+        js.addBinding("Application", new ApplicationBinding(js));
+        js.executeScript("lib/js/Деденево/3D/Модель3D.js");
 
         //js.addBinding(new ObjBinding());
 
         js.loadLibrary("lib/js/БиблиотекаФункций.js");
         js.loadLibrary("lib/js/СопротивлениеМатериалов.js");
-      //  js.loadLibrary("lib/js/ЖелезнаяБалка.js");
-      //  js.loadLibrary("lib/js/Железобетон.js");
+        js.loadLibrary("lib/js/ЖелезнаяБалка.js");
+        js.loadLibrary("lib/js/Железобетон.js");
         js.loadLibrary("lib/js/Теплотехника.js");
 
      //   js.executeScript("lib/js/Деденево/РасчетКрыльца.js");
-        js.executeScript("lib/js/Деденево/РасчетСтен.js");
+     //   js.executeScript("lib/js/Деденево/РасчетСтен.js");
      //   js.executeScript("lib/js/Деденево/РасчетКрыши.js");
 
       //   js.executeScript("lib/js/Деденево/Крыша3D.js");
